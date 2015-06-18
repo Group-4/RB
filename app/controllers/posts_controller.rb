6fa77a48b154bc11be567af9bdbe646cd5cc16b2
Taylor Daugherty <mdaugherty6@gmail.com>
2015-06-18 10:10:42 -0400

    fixed post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,12 @@ class PostsController < ApplicationController
 		if current_user 
 			url = params[:image_url]
 			answer = params[:answer]
-			@post = Post.create(image_url: url, user_id: current_user.id, answer: params[:answer], hint: params[:hint], solved: false)
-			render "create.json.jbuilder", status: :created
+			@post = Post.new(image_url: url, user_id: current_user.id, answer: params[:answer], hint: params[:hint], solved: false)
+			if @post.save 
+				render "create.json.jbuilder", status: :created
+			else 
+				render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+      end
 		else
 			authenticate_with_token!
 		end
