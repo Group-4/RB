c83f6413c46e51338636aaefd7757095bd8bc719
WebEpic <randy@randyjmitchell.com>
2015-06-18 18:48:39 -0400

    added delete route and method to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,32 @@
 class PostsController < ApplicationController
 
-	def create
-		if current_user 
-			url = params[:image_url]
-			answer = params[:answer]
-			@post = Post.new(image_url: url, user_id: current_user.id, answer: params[:answer], hint: params[:hint], owner: current_user.username, solved:nil)
-			if @post.save 
-				render "create.json.jbuilder", status: :created
-			else 
-				render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+    def create
+        if current_user 
+            url = params[:image_url]
+            answer = params[:answer]
+            @post = Post.new(image_url: url, user_id: current_user.id, answer: params[:answer], hint: params[:hint], owner: current_user.username, solved:nil)
+            if @post.save 
+                render "create.json.jbuilder", status: :created
+            else 
+                render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
       end
-		else
-			authenticate_with_token!
-		end
-	end
-	
-	def index
-		page_num = params[:page] || 1
-		@posts = Post.order(created_at: :desc).page(page_num)
-	end
+        else
+            authenticate_with_token!
+        end
+    end
+    
+    def index
+        page_num = params[:page] || 1
+        @posts = Post.order(created_at: :desc).page(page_num)
+    end
 
-	def get 
-		@post = Post.find(params[:id])
-	end
-	
+    def get 
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+      @post = Post.find_by(id: params[:id])
+      @post.destroy
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ scope defaults: { format: 'json'} do
 	get 'posts', to: 'posts#index'
 	post 'posts', to: 'posts#create'
 	get 'posts/:id', to: 'posts#get'
+	delete 'posts/:id', to: 'posts#destroy'
 
 	##guess routes
 	get 'guesses', to: 'guesses#index'
