b7345b1ca6d0781e95bdf9eeb0d5a2ce51de74ea
WebEpic <randy@randyjmitchell.com>
2015-06-19 12:36:26 -0400

    removed special chars from guesses and answers of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,10 @@ class PostsController < ApplicationController
       if current_user 
         url = params[:image_url]
         answer = params[:answer]
-        @post = Post.new(image_url: url, user_id: current_user.id, answer:params[:answer], hint: params[:hint], owner: current_user.username, solved:nil)
+        answer.gsub!(/[^0-9A-Za-z]/, '')
+
+        @post = Post.new(image_url: url, user_id: current_user.id, answer: answer, hint: params[:hint], owner: current_user.username, solved:nil)
+        
         if @post.save 
         	render "create.json.jbuilder", status: :created
         else 
