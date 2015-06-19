62ecf8457cd906df038c221ddeba2c51c79f75b6
Taylor Daugherty <mdaugherty6@gmail.com>
2015-06-19 11:12:02 -0400

    changed regex for post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :guesses, dependent: :destroy
 	validates :image_url, :answer, presence: true
-	validates :image_url, format: {with:/\.(png|jpg|gif)$/, message: "not a valid image_url"}
+	validates :image_url, format: {with:/\.(png|jpg|gif)\z/, message: "not a valid image_url"}
 
 end
