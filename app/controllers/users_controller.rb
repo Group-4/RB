ad7aecc6224bfbcd714994a8fc3b6fdce97e8935
Taylor Daugherty <mdaugherty6@gmail.com>
2015-06-21 17:52:31 -0400

    added guess_count, incorrect_count, correct_count, and win_percent to user.  changed sorting of leaderboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,20 @@ class UsersController < ApplicationController
   before_action :authenticate_with_token!, only: [:index, :find, :posts, :logged_in_user]
 
   def register
-    passhash = Digest::SHA1.hexdigest(params[:password])
-    @user = User.new(username: params[:username], email: params[:email], password: passhash)
-    if @user.save
-      render "register.json.jbuilder", status: :created
+    password = params[:password]
+    username = params[:username]
+    email = params[:email]
+    if password && username && email
+      passhash = Digest::SHA1.hexdigest(password)
+      @user = User.new(username: username, email: email, password: passhash)
+      if @user.save
+        render "register.json.jbuilder", status: :created
+      else
+        render json: { errors: @user.errors.full_messages },
+          status: :unprocessable_entity
+      end
     else
-      render json: { errors: @user.errors.full_messages },
-        status: :unprocessable_entity
+      render json: {msg: "Please provide username, password, and email."}, status: :unprocessable_entity
     end
   end
 
@@ -79,8 +86,14 @@ class UsersController < ApplicationController
   end
 
   def leaderboard
-    @users = User.order(points: :desc).page(1).per(10)
-    render status: :created
+    sort = params[:sort]
+    if sort == "ratio"
+      @users = User.where('correct_count > 1').order(win_percent: :desc).page(1).per(10)
+      render status: :created
+    else
+      @users = User.order(points: :desc).page(1).per(10)
+      render status: :created
+    end
   end
 
   def guesses
