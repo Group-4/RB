87f6c9136bbae167bc0f337f6365b263f9f91a6e
Taylor Daugherty <mdaugherty6@gmail.com>
2015-06-21 20:31:03 -0400

    changed scoring

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -8,15 +8,21 @@ def check_solution
 		self.user.update(guess_count: self.user.guess_count + 1)
 		if self.guess == self.post.answer
 			self.update(correct: true)
+			self.post.update(times_solved: self.post.times_solved + 1 )
 			self.user.update(correct_count: self.user.correct_count + 1 )
 			win_percent = (self.user.correct_count.to_f/self.user.guess_count)*100
 			self.user.update(win_percent: win_percent)
-			if self.post.solved
-				self.user.update(points: self.user.points + 50)
-			else 
+			case self.post.times_solved
+			when 1
+				self.user.update(points: self.user.points + 200)
 				self.post.update(solved: self.user.id)
 				self.post.update(solved_by: self.user.username)
-				self.user.update(points: self.user.points + 200)
+			when 2
+				self.user.update(points: self.user.points + 100)
+			when 3
+				self.user.update(points: self.user.points + 75)
+			else 
+				self.user.update(points: self.user.points + 50)
 			end
 		else
 			self.post.update(attempts: self.post.attempts + 1)
