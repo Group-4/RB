b7ac1c0f5543b2445246ad93802719816daf7e31
Taylor Daugherty <mdaugherty6@gmail.com>
2015-06-24 00:42:12 -0400

    Added tests

--- test/models/guess_test.rb
+++ test/models/guess_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class GuessTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "correct changes to true if guess is same as answer" do 
+	 user = User.find_or_create_by(email:"test2@email.com", username:"test2", password:"test", access_token: "8f8f6513553acb816da66077486f370b")
+	 binding.pry
+	 post = Post.find_or_create_by(answer:"testing", image_url:"test.jpg",user_id:user.id)
+	 guess = Guess.create(guess: "testing", post_id:post.id, user_id:user.id)
+	 guess.check_solution
+	 assert guess.correct
+	end
+	
 end
