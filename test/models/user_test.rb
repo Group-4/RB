d22ee51bac47b6cd760346847b3cf434dde3b3eb
WebEpic <randy@randyjmitchell.com>
2015-06-24 09:23:35 -0400

    testing username, email and password presence: true. these tests are passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,17 +5,17 @@ class UserTest < ActiveSupport::TestCase
   user = User.new(username: "username", email: "email@truth.com", password: "passhash")
   
   test "username required to register" do
-    user.username = "       "
+    user.username = "username"
     assert user.valid?
   end
 
   test "email required to register" do
-    user.email = "       "
+    user.email = "test@truth.com"
     assert user.valid?
   end
 
   test "password required to register" do
-    user.password = "       "
+    user.password = "passhash"
     assert user.valid?
   end
 end
