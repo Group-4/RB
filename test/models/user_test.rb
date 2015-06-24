081c3099cd9f2cd44eaf80e652380561a4087b1c
WebEpic <randy@randyjmitchell.com>
2015-06-24 09:18:24 -0400

    testing username, email and password presence: true. these tests are failing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,17 +5,17 @@ class UserTest < ActiveSupport::TestCase
   user = User.new(username: "username", email: "email@truth.com", password: "passhash")
   
   test "username required to register" do
-    user.username = "username"
+    user.username = "       "
     assert user.valid?
   end
 
   test "email required to register" do
-    user.email = "test@truth.com"
+    user.email = "       "
     assert user.valid?
   end
 
   test "password required to register" do
-    user.password = "passhash"
+    user.password = "       "
     assert user.valid?
   end
 end
